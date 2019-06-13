f4a45dc7b2b760e75d24568bf601d348d91d0fd9
Eundo Lee <cobed95@gmail.com>
2019-06-13 11:21:46 +0000

    second test with default DAG preconstructed

--- run_and_compare.sh
+++ run_and_compare.sh
@@ -1,3 +1,7 @@
+echo "Running with custom DAG construction result..."
 ./daf_1min -d ./data/human -q ./data/human_40n -a ./data/human_40n.dag -n 100 > result1
-./daf_1min -d ./data/human -q ./data/human_40n -n 100 > result2
+echo "Done"
+echo "Running with default DAG construction result..."
+./daf_1min -d ./data/human -q ./data/human_40n -a ./data/default.dag -n 100 > result2
+echo "Done"
 python sort_result.py result1 result2
